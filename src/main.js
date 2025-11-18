cf3f95ecaaf09abb2ce4452e5262389905775267
panktiB <mishthii95@hotmail.com>
2025-11-18 16:19:02 +0530

    create: initial commit

---
+++ src/main.js
@@ -0,0 +1,38 @@
+import './assets/main.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  themes: {
+    myTheme: {
+      dark: false,
+      colors: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        background: '#f5f5f5',
+      },
+    },
+  },
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
