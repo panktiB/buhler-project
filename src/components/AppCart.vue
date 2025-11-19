dd94fc2596f1f4a10a2ba9571af4db2117472b07
panktiB <mishthii95@hotmail.com>
2025-11-19 11:16:26 +0530

    update: displaying basic cart items on page

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -1,3 +1,18 @@
-<template>cart</template>
+<template>
+  <v-container>
+    <v-row v-for="item in items" :key="item.id" class="mb-4">
+      <v-col cols="12">
+        <div class="">{{item.name}}</div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-<script setup></script>
+<script setup>
+import { useCartStore } from '@/stores/cart.js'
+import { onMounted, ref, computed } from 'vue'
+
+const cartStore = useCartStore()
+
+const items = computed(() => cartStore.products)
+</script>
