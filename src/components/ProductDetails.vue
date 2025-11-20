513a97af600d54fcb18a89e492467d8d550b2b70
panktiB <mishthii95@hotmail.com>
2025-11-20 12:08:44 +0530

    update: minor fixes to details page

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,15 +1,38 @@
 <template>
   <v-container>
     <v-row>
-      <v-btn variant="plain" class="text-capitalize text-secondary" @click="$router.back()">
-        Back
-      </v-btn>
-    </v-row>
-    <v-row class="border-primary">
-      <v-col cols="3" class="bg-white">
-        <v-img :src="product?.imageUrl" height="300" />
+      <v-col class="px-0">
+        <v-btn
+          variant="plain"
+          class="text-capitalize px-0 text-secondary"
+          prepend-icon="mdi-arrow-left"
+          @click="$router.back()"
+        >
+          Back
+        </v-btn>
       </v-col>
-      <v-col class="text-secondary pl-8" cols="9">
+    </v-row>
+
+    <!-- Loading State -->
+    <v-row v-if="loading" class="justify-center my-12">
+      <v-col cols="auto">
+        <v-progress-circular indeterminate color="primary" />
+      </v-col>
+    </v-row>
+
+    <!-- Error/Not Found State -->
+    <v-row v-else-if="error">
+      <v-col cols="12">
+        <v-alert type="error" variant="tonal"> Product not found </v-alert>
+      </v-col>
+    </v-row>
+
+    <!-- Product Details -->
+    <v-row v-else class="border-primary">
+      <v-col cols="12" md="3" class="bg-white">
+        <v-img :src="product?.imageUrl" :alt="product?.name" height="300" />
+      </v-col>
+      <v-col class="text-secondary pl-8" cols="12" md="9">
         <v-row>
           <div class="text-h6 font-weight-bold pl-5">{{ product?.name }}</div>
         </v-row>
@@ -22,8 +45,13 @@
           <div class="pl-5">x{{ product?.price }}</div>
         </v-row>
         <v-row>
-          <v-btn variant="text" @click.stop="handleClick" class="text-capitalize">
-            <v-icon>mdi-cart-outline</v-icon>
+          <v-btn
+            variant="text"
+            color="secondary"
+            prepend-icon="mdi-cart-outline"
+            class="text-capitalize"
+            @click="addToCart"
+          >
             Add to cart
           </v-btn>
         </v-row>
@@ -42,6 +70,10 @@ const route = useRoute()
 const cartStore = useCartStore()
 
 const productId = ref()
+
+const loading = computed(() => productsStore.loading)
+const error = computed(() => productsStore.error)
+
 const product = computed(() => productsStore.getProductById(productId.value))
 
 const productsStore = useProductsStore()
@@ -51,7 +83,7 @@ onMounted(() => {
   productsStore.initialize()
 })
 
-const handleClick = () => {
+const addToCart = () => {
   cartStore.addProduct(product.value)
 }
 </script>
