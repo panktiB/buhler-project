d1382b854908c593310e3e6f1b679c902a78b4d7
panktiB <mishthii95@hotmail.com>
2025-11-20 16:22:27 +0530

    update: removed unused packages

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    vuetify({ autoImport: true }), // Add this
+    vuetify({ autoImport: true }),
   ],
   resolve: {
     alias: {
