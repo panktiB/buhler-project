3f806b3c4f1228cbb620d293662b3cb1ce5d5a21
panktiB <mishthii95@hotmail.com>
2025-11-20 10:09:43 +0530

    update: basic test cases for AppCart

--- src/components/__tests__/AppCart.test.js
+++ src/components/__tests__/AppCart.test.js
@@ -1,7 +1,28 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
+import AppCart from '../AppCart.vue'
+import { useCartStore } from '@/stores/cart'
+import { setupPinia, mountWithPinia, cleanupWrapper } from '@/tests/testUtils'
 
 describe('AppCart.vue', () => {
-  it('mounts properly', () => {
-    expect(true).toBe(true)
+  let wrapper
+
+  afterEach(() => {
+    cleanupWrapper(wrapper)
   })
+
+  it('renders without errors', () => {
+    mountWithPinia(AppCart)
+  })
+
+  it('renders checkout title', () => {
+    wrapper = mountWithPinia(AppCart)
+    expect(wrapper.text()).toContain('Checkout')
+  })
+
+  it('displays correct total without any cart items', () => {
+    wrapper = mountWithPinia(AppCart)
+    expect(wrapper.text()).toContain('Total: x0')
+  })
+
+
 })
